3b37e39aefdde2e2105b256e11cb1cce7706860f
Âu Duy Thông <auduythong1610@gmail.com>
2022-01-03 16:26:38 +0700

    update profile

--- main.js
+++ main.js
@@ -1,3 +1,42 @@
+/* Toggler Navbar*/
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", () => {
+  hideSection();
+  togglerNavbar();
+  document.body.classList.toggle("hide-scrolling");
+});
+function hideSection() {
+  document.querySelector("section.active").classList.toggle("fade-out");
+}
+
+function togglerNavbar() {
+  document.querySelector(".header").classList.toggle("active");
+}
+
+/* Active section */
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("link-item") && e.target.hash !== "") {
+    document.querySelector(".overlay").classList.add("active");
+    navToggler.classList.add("hide");
+    if (e.target.classList.contains("nav-item")) {
+      togglerNavbar();
+    } else {
+      hideSection();
+      document.body.classList.add("hide-scrolling");
+    }
+    setTimeout(() => {
+      document
+        .querySelector("section.active")
+        .classList.remove("active", "fade-out");
+      document.querySelector(e.target.hash).classList.add("active");
+      window.scrollTo(0, 0);
+      document.body.classList.remove("hide-scrolling");
+      navToggler.classList.remove("hide");
+      document.querySelector(".overlay").classList.remove("active");
+    }, 500);
+  }
+});
+
 /* About tabs */
 
 const tabContainer = document.querySelector(".about-tabs"),
